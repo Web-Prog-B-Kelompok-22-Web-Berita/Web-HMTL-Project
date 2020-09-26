0d14e611053d7bcda8bc75779de7055e260290db
Frtsnts10 <Frtsnts10@gmail.com>
2020-09-26 08:47:11 +0700

    Update 26/09
    Folder Double gua hapus, yang kmrn dipush gua udh pull uda gua cek. jd skrg tinggal 1 folder sama 1 folder utk genre.

--- Tampilan abis di edit Martin/scroll.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("scroll", event => {
-    let scrollTop = document.documentElement.scrollTop;
-    let scrollHeight = document.documentElement.scrollHeight - 
-document.documentElement.clientHeight;
-    let progress = 100 * scrollTop / scrollHeight;
-    document.querySelector(".progress").style.width = progress + "%";
-})
